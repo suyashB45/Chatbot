6d919aa9602b90cde7c67970d9fe5d0b138df49a
Suyash B <suyashbalasubramaniam@gmail.com>
2024-12-12 13:37:54 +0530

    app.py

--- app.py
+++ app.py
@@ -44,12 +44,14 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+Use the provided context to answer the question as comprehensively as possible. Include all relevant details from the context in your response. If the answer is not available in the context, respond with: "The answer is not available in the context." Avoid providing incorrect or speculative answers.
 
-    Answer:
+Context: {context}
+
+Question: {question}
+
+Answer:
+
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
